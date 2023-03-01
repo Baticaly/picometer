2ca4ecf5104af798e5b2ca63fc8af4cfd9610497
Batuhan Ergun <ergunbatuhan@icloud.com>
2023-03-01 04:06:55 +0100

    channel value parse & final graphs

--- public/javascripts/WebSerial.js
+++ public/javascripts/WebSerial.js
@@ -27,7 +27,6 @@ async function getReader() {
 
             if (values.length > 1) {
                 lineBuffer = values.pop();
-
                 let channels = values.pop().trim().split('-')
                 ADC0.data.datasets[0].data.push({ x: Date.now(), y: channels[0] });
                 ADC1.data.datasets[0].data.push({ x: Date.now(), y: channels[1] });
